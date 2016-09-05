9bfd51e853adeb5651538382321dcfc18c1a25ca
Ian Munro <i.munro@imperial.ac.uk>
2016-09-05 22:37:23 +0100

    Add option to use pqfogim queue.

--- ChainScript.sh
+++ ChainScript.sh
@@ -3,13 +3,27 @@
 #  Created by Ian Munro on 01/09/2016.
 #
 
+if [ "$1" = "pqfogim" ]
+then
+echo "fogim queue"
+one=$(qsub -q pqfogim ARRVNCScript.pbs)
+echo "launching processing job"
+echo $one
+two=$(qsub -q pqfogim -W depend=afterok:$one MERGEScript.pbs )
+echo "launching merge job"
+echo $two
 
-one=$(qsub ARRVNCScript.pbs)
+else
+
+one=$(qsub ARRVNCScript.pbs )
 echo "launching processing job"
 echo $one
 two=$(qsub -W depend=afterok:$one MERGEScript.pbs)
 echo "launching merge job"
 echo $two
+
+fi
+
 
 
 
