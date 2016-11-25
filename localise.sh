a20692e204303f9a6fd007e2746db82270bf5b80
Ian Munro <i.munro@imperial.ac.uk>
2016-11-25 13:20:54 +0000

    Replace chainscript with less cryptically-named “localise.sh” and also make pqfogim queue the default.

--- localise.sh
+++ localise.sh
@@ -3,22 +3,23 @@
 #  Created by Ian Munro on 01/09/2016.
 #
 
-if [ "$1" = "pqfogim" ]
+if [ "$1" = "general" ]
 then
-echo "fogim queue"
-one=$(qsub -q pqfogim ARRVNCScript.pbs)
-echo "launching processing job"
-echo $one
-two=$(qsub -q pqfogim -W depend=afterok:$one MERGEScript.pbs )
-echo "launching merge job"
-echo $two
-
-else
 
 one=$(qsub ARRVNCScript.pbs )
 echo "launching processing job"
 echo $one
 two=$(qsub -W depend=afterok:$one MERGEScript.pbs)
+echo "launching merge job"
+echo $two
+
+else
+
+echo "fogim queue"
+one=$(qsub -q pqfogim ARRVNCScript.pbs)
+echo "launching processing job"
+echo $one
+two=$(qsub -q pqfogim -W depend=afterok:$one MERGEScript.pbs )
 echo "launching merge job"
 echo $two
 
