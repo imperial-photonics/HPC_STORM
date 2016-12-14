1f0e6129918dabca479831f138affd5988006ee1
Ian Munro <i.munro@imperial.ac.uk>
2016-12-14 16:49:59 +0000

    Add script to setup a VNC connection N.B. Display NO is hard-coded (to :6 in example)

---
+++ LAUNCH_VNC_Display.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#  Created by Ian Munro on 25/11/2016.
+#
+
+##load application modules
+module load fiji vnc
+
+#check to see if "my" display is already in use
+
+MY=$(vncserver -list | grep :6)
+MY=${MY:0:2}
+
+if [ -n "$MY" ]; then
+echo "Unable to start display " $MY
+echo "Requested display is already in use!"
+
+else
+
+vncserver :6
+
+MY=$(vncserver -list | grep :6)
+MY=${MY:0:2}
+
+echo "Started display "$MY
+echo "Please connect to port 5906 "
+
+read -p "Shut down display now? " answer
+
+vncserver -kill $MY
+
+rm core.*
+rm .vnc/*.log
+
+fi
+
+
