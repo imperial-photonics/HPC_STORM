25e67e36959d5418b5b4473bc1e83c3a09b2a74b
Ian Munro <i.munro@imperial.ac.uk>
2017-04-08 13:51:04 +0100

    Add new overall script to produce preview images.

---
+++ PREVIEW.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+#  Created by Ian Munro on 22/03/2017.
+#
+
+USAGE="Usage: PREVIEW filename <calibration_filename> "
+
+
+case "$#" in
+1)
+   ARGS="$WORK":"$1":"$HOME"
+   ;;
+2)
+   ARGS="$WORK":"$1":"$2":"$HOME"
+   ;;
+*)
+   echo $USAGE;
+   exit 0
+   ;;
+esac
+
+echo $ARGS
+
+read -p "Please enter Lateral uncertainty [nm] ? " answer
+
+echo $answer
+
+
+
+cd $HOME/Localisation
+
+echo "fogim queue"
+one=$(qsub -q pqfogim -v SETUP_ARGS=$ARGS $HOME/Localisation/setupScript.pbs)
+echo "launching setup job"
+echo $one
+two=$(qsub -q pqfogim -W depend=afterok:$one $HOME/Localisation/loc_ARRScript.pbs)
+echo "launching Localisation processing job"
+echo $two
+three=$(qsub -q pqfogim -W depend=afterok:$two $HOME/Localisation/loc_MERGEScript.pbs )
+echo "launching localisation merge job"
+echo $three
+
+cd $HOME/Visualisation
+
+four=$(qsub -q pqfogim -v LATERAL_RES=$answer,POST="SIGMA_FILTER" -W depend=afterok:$three $HOME/Visualisation/vis_Script.pbs )
+echo "launching sigma filtering job"
+echo $four
+five=$(qsub -q pqfogim -v LATERAL_RES=$answer,POST="DRIFT" -W depend=afterok:$three $HOME/Visualisation/vis_Script.pbs )
+echo "launching drift correction job"
+echo $five
+
+
+
+
+
+
+
+
+
