317357cce1c15d2ea0d36f8854e101305225bb2e
Mark Neil <mark.neil@imperial.ac.uk>
2018-04-24 14:19:53 +0100

    More script tests

--- Localisation/loc_NodeScript.sh
+++ Localisation/loc_NodeScript.sh
@@ -23,94 +23,67 @@ IJ=/apps/fiji/Fiji.app/ImageJ-linux64
 #HOME=/Users/imunro/HPC_STORM
 #IJ=/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx
 
-ARGS="$1"
-
-ARRARGS=(${ARGS//:/ })
-INPATH=${ARRARGS[0]}
-FNAME=${ARRARGS[1]}
-
-if [ ${#ARRARGS[@]} == "5" ];then
-  NJOBS=${ARRARGS[3]}
-else
-  CALIB=${ARRARGS[2]}
-  NJOBS=${ARRARGS[4]}
-fi
-
-# add environment variables to args list
-ARGS_FULL="$1":"$TMPDIR"
-
 # if NJOBS == 1 &&  jobs and PBS_ARRAY_INDEX > 1
 # then this is a dummy array job so do nothing
-if [[ $NJOBS  != 1  ]] || [[  $PBS_ARRAY_INDEX == 1 ]]
-then
+if [[ $NJOBS  != 1  ]] || [[  $PBS_ARRAY_INDEX == 1 ]]; then
 
-  ARRFNAME=(${FNAME//.ome/ })
-  NAME=${ARRFNAME[0]}
+    ARRFNAME=(${FNAME//.ome/ })
+    NAME=${ARRFNAME[0]}
 
-
-  # split name of temp dir to get pbsjob no
-  ARR=(${TMPDIR//./ })
-  STR=${ARR[1]}
-  ARR=(${STR//[/ })
-  JOBNO=${ARR[0]}
+    # split name of temp dir to get pbsjob no
+    ARR=(${TMPDIR//./ })
+    STR=${ARR[1]}
+    ARR=(${STR//[/ })
+    JOBNO=${ARR[0]}
   
-  
-  echo "Jobno = "
-  echo $JOBNO
+    echo "Jobno = "
+    echo $JOBNO
 
+    #create a subdir to hold the outputs from this job
 
-  #create a subdir to hold the outputs from this job
-
-  if [ ! -d $INPATH/$JOBNO ]
-  then
-    mkdir $INPATH/$JOBNO
-  fi
-
-  #if current data file does not exist then copy
-  if [ ! -f $TMPDIR"/"$FNAME ]
-  then
-    rm ${TMPDIR}"/*"
-    if [[ $INPATH == "/external"* ]]
-    then
-      echo "secure copying data file"
-      scp -q ${USER}@login-2-internal:${INPATH}/${NAME}*.ome.tif ${TMPDIR}
-      # ssh ${USER}@login-2-internal “cd ${INPATH};tar zcf ${NAME}*.ome.tif ” | tar  zxf –
-    else
-      echo "copying data file"
-      cp ${INPATH}/${NAME}*.ome.tif ${TMPDIR}
+    if [ ! -d $INPATH/$JOBNO ]; then
+        mkdir $INPATH/$JOBNO
     fi
-  fi
+
+    #if current data file does not exist then copy
+    if [ ! -f $TMPDIR"/"$FNAME ]; then
+        rm ${TMPDIR}"/*"
+        if [[ $INPATH == "/external"* ]]; then
+            echo "secure copying data file"
+            scp -q ${USER}@login-2-internal:${INPATH}/${NAME}*.ome.tif ${TMPDIR}
+            # ssh ${USER}@login-2-internal “cd ${INPATH};tar zcf ${NAME}*.ome.tif ” | tar  zxf –
+        else
+            echo "copying data file"
+            cp ${INPATH}/${NAME}*.ome.tif ${TMPDIR}
+        fi
+    fi
   
-  if [ ! -f $TMPDIR"/"$FNAME ]
-  then
-    echo "Copy failed!"
-    exit 0
-  fi
+    if [ ! -f $TMPDIR"/"$FNAME ]; then
+        echo "Copy failed!"
+        exit 0
+    fi
     
 
-  if [ ${#ARRARGS[@]} == "6" ]
-  then
-    if [ ! -f $TMPDIR"/"$CALIB ]
-    then
-      if [[ $INPATH == "/external"* ]]
-      then
-        echo "secure copying calibration file"
-        scp -q ${USER}@login-2-internal:${INPATH}/${CALIB} ${TMPDIR}
-      else
-        echo "copying calibration file"
-        cp ${INPATH}/${CALIB} ${TMPDIR}
-      fi
+    if [ $THREED==1 ]; then
+        if [ ! -f $TMPDIR"/"$CALIB ]; then
+            if [[ $INPATH == "/external"* ]]; then
+                echo "secure copying calibration file"
+                scp -q ${USER}@login-2-internal:${INPATH}/${CALIB} ${TMPDIR}
+            else
+                echo "copying calibration file"
+                cp ${INPATH}/${CALIB} ${TMPDIR}
+            fi
+        fi
     fi
-  fi
 
-  module load sysconfcpus/0.5
+    module load sysconfcpus/0.5
 
-  # run ThunderSTORM
+    # run ThunderSTORM
   
-  # sysconfcpus -n $NCPUS $IJ --ij2 -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
-  sysconfcpus -n 48 fiji --ij2 -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
-  # sysconfcpus -n 24 fiji -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
-  #echo "returned from Macro"
+    # sysconfcpus -n $NCPUS $IJ --ij2 -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
+    sysconfcpus -n 48 fiji --ij2 -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
+    # sysconfcpus -n 24 fiji -macro $HOME/Localisation/TSTORM_loc_macro.ijm $ARGS_FULL
+    #echo "returned from Macro"
 
 
 awk -v job_index=$PBS_ARRAY_INDEX -v job_no=$NJOBS 'BEGIN{FS=",";OFS=",";OFMT="%.2f"; getline }{$2=job_no*($2-1)+job_index; print $0}' $TMPDIR/tmp_"$NAME"_slice_$PBS_ARRAY_INDEX.csv  > $WORK/$JOBNO/tmp_"$NAME"_$PBS_ARRAY_INDEX.csv
