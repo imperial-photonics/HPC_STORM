a37b46892718885659ead065c5300350b786fe2d
Mark Neil <mark.neil@imperial.ac.uk>
2018-04-24 12:52:08 +0100

    More script tests

--- Localisation/LAUNCH_localise.sh
+++ Localisation/LAUNCH_localise.sh
@@ -13,34 +13,55 @@
 
 USAGE="Usage: LAUNCH_localise [-b] filename(inc path) [calibration_file(name only - must be in same directory] <-b> "
 
-function parse {
-  export INPATH=$(dirname "${FULLNAME}")
-  export FNAME=$(basename "${FULLNAME}")
-  if [[ $(hostname -s) == "login-2-internal" ]]
-  then
-    if [ -f ${FULLNAME} ]
+function parse_and_check {
+    export INPATH=$(dirname "${FULLNAME}")
+    export FNAME=$(basename "${FULLNAME}")
+    if [[ $(hostname -s) == "login-2-internal" ]]
     then
-      echo "File found!"
+        if [ -f ${FULLNAME} ]
+        then
+            echo "File found!"
+        else
+            echo "Error! File not found!"
+            exit 0
+        fi
     else
-      echo "Error! File not found!"
-      exit 0
+        COMMAND="[ -f "${FULLNAME}" ]"
+        if ssh ${USER}@login-2-internal ${COMMAND}
+        then
+            echo "File found!"
+        else
+            echo "Error! File not found!"
+            exit 0
+        fi
     fi
-  else
-    COMMAND="[ -f "${FULLNAME}" ]"
-    if ssh ${USER}@login-2-internal ${COMMAND}
+    if [ $THREED == 1 ]
     then
-      echo "File found!"
-    else
-      echo "Error! File not found!"
-      exit 0
+        if [[ $(hostname -s) == "login-2-internal" ]]
+        then
+            if [ -f ${INPATH}/${CALIB} ]
+            then
+                echo "Calibration file found!"
+            else
+                echo "Error!  Calibration file not found!"
+                exit 0
+            fi
+        else
+            COMMAND="[ -f "${INPATH}"/"${CALIB}" ]"
+            if ssh ${USER}@login-2-internal ${COMMAND}
+            then
+                echo "Calibration file found!"
+            else
+                echo "Error!  Calibration file not found!"
+                exit 0
+            fi
+        fi
     fi
-  fi
 }
 
 
 echo "fogim queue"
 QUEUE="pqfogim"
-
 
 FULLNAME=$1
 INPATH=""
@@ -49,92 +70,66 @@ FNAME=""
 echo "How many HPC nodes do you wand to use?"
 read -p "Enter number of nodes on the HPC: " NJOBS
 
-THREED=0
+export NJOBS
+export THREED=0
 
 case "$#" in
-1)
-parse
-ARGS="$INPATH":"$FNAME"
-;;
-2)
-  parse
-  if [ $2 == "-b" ]
-  then 
-    NJOBS=1
-    ARGS="$INPATH":"$FNAME"
-  else
-    THREED=1
-    ARGS="$INPATH":"$FNAME":"$2"
-  fi
-;;
-3)
-  parse
-  if [ $3 == "-b" ]
-  then 
-    THREED=1
-    NJOBS=1
-    ARGS="$INPATH":"$FNAME":"$2"
-  else
-    echo $USAGE;
-    exit 0
-  fi
-;;
-*)
-echo $USAGE;
-exit 0
-;;
+    1)
+        ;;
+    2)
+        if [ $2 == "-b" ]
+        then
+            NJOBS=1
+        else
+            THREED=1
+            export CALIB=$2
+        fi
+        ;;
+    3)
+        if [ $3 == "-b" ]
+        then
+            THREED=1
+            NJOBS=1
+            export CALIB=$2
+        else
+            echo $USAGE;
+            exit 0
+        fi
+        ;;
+    *)
+        echo $USAGE;
+        exit 0
+        ;;
 esac
 
 ## check calibration file , if needed, exists
-if [ $THREED == 1 ]
-then
-  if [[ $(hostname -s) == "login-2-internal" ]]
-  then
-    if [ -f ${INPATH}/${2} ]
-    then
-      echo "Calibration file found!"
-    else
-      echo "Error!  Calibration file not found!"
-      exit 0
-    fi
-  else
-    COMMAND="[ -f "${INPATH}"/"${2}" ]"
-    if ssh ${USER}@login-2-internal ${COMMAND}
-    then
-      echo "Calibration file found!"
-    else
-      echo "Error!  Calibration file not found!"
-      exit 0
-    fi
-  fi
-fi
 
-
+parse_and_check
 
 # set  work directory and no of jobs
 ARGS="$ARGS":"$WORK":"$NJOBS"
 
-#   $ARGS now contains the arguements to pass to the localisation script as a colon separated list.  THese are either
-#   ARGS = {input file directory}:{input file name}:{working directory}:{numbero of jobs}
-#   or
-#   ARGS = {input file directory}:{input filename}:{calibration filename}:{working directory}:{number of jobs}
-#   where does $THREED go?
+#   environment variables $INPATH $FNAME $NJOBS $THREED $CALIB now contain the necessary information for the other scrips to work
 
 if [ $NJOBS == "1" ]
 then
-  one=$(qsub -q $QUEUE -v SETUP_ARGS=$ARGS $HOME/Localisation/loc_ARRScriptSingle.pbs)
+  one=$(qsub -q $QUEUE $HOME/Localisation/loc_ARRScriptSingle.pbs)
 else
-  one=$(qsub -q $QUEUE -v SETUP_ARGS=$ARGS $HOME/Localisation/loc_ARRScript.pbs)
+  one=$(qsub -q $QUEUE $HOME/Localisation/loc_ARRScript.pbs)
 fi
 echo "launching processing job"
 echo $one
 
 # split name of first job to get pbsjob no
 ARR=(${one//[/ })
-JNO=${ARR[0]}
+export JOBNO=${ARR[0]}
 
 echo "Please enter Lateral uncertainty [nm] ? "
 read -p " enter zero to disable preview images ? " lateral
+
+export LATERAL_RES=lateral
+export POST_PROC="DRIFT"
+#   environment variables $JOBNO, $POST_PROC and $LATERAL_RES are exported for use in merge and post processing scripts
 
 two=$(qsub -q $QUEUE -W depend=afterok:$one -v SETUP_ARGS=$ARGS,JOBNO=$JNO,LATERAL_RES=$lateral,POST="DRIFT" $HOME/Localisation/loc_MERGEScript.pbs)
 
